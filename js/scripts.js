47dcaf1142e7be848b2ccb92dca3ebfb63bf523e
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-19 10:44:41 -0700

    WIP determine if any number in array contains 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   document.querySelector("form").addEventListener("submit", counting);
-  });
+});
 // business logic
 function counting(event) {
   event.preventDefault();
   const countTo = parseInt(document.getElementById("countTo").value);
   const finalNumb = []
   for (let index = 0; index <= countTo; index += 1) {
-  finalNumb.push(index);
-  if (finalNumb.includes(1) )
+    finalNumb.push(index);
+    }
+  function includes1(element) {
+    if (finalNumb.includes(1))
+    return true;
+     
   };
-};
 
 
 
 
-// ui
+
+
+
+  // ui
   document.getElementById("result").innerText = finalNumb
+};
