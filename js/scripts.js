3a93e2f626bd07a69aa442bbae535c5c32356848
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-19 08:51:59 -0700

    return an array of numbers from 0 to the user's inputted number

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", counting);
+  });
+// business logic
+function counting(event) {
+  event.preventDefault();
+  const countTo = parseInt(document.getElementById("countTo").value);
+  const finalNumb = []
+  for (let index = 0; index <= countTo; index += 1) {
+  finalNumb.push(index);
+}
+
+
+
+
+// ui
+  document.getElementById("result").innerText = finalNumb
+}
