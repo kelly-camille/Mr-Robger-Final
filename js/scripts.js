64d4ded5fd4f5dc7fae77e6b2a66b80347df8fc5
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-20 14:33:12 -0700

    correct spacing and indentation

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,18 @@ window.addEventListener("load", function () {
 function counting(event) {
   event.preventDefault();
   const countTo = parseInt(document.getElementById("countTo").value);
-  
+
   const numArr = []
   for (let index = 0; index <= countTo; index += 1) {
     const str = index.toString();
     if (str.includes("3") === true) {
-    numArr.push("Won't you be my neighbor?");
+      numArr.push("Won't you be my neighbor?");
     } else if (str.includes("2") === true) {
-    numArr.push("Boop!")
+      numArr.push("Boop!")
     } else if (str.includes("1") === true) {
-      numArr.push ("Beep!")
+      numArr.push("Beep!")
     } else {
-    numArr.push(index);
+      numArr.push(index);
     }
   }
   // ui
