c3fcccd70f74dcc24e86137c917a141f9a25c667
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-20 15:12:23 -0700

    edit separation of ui and bl

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 window.addEventListener("load", function () {
-  document.querySelector("form").addEventListener("submit", counting);
+  document.querySelector("form").addEventListener("submit", handlesEvent);
+
 });
-// business logic
-function counting(event) {
-  event.preventDefault();
-  const countTo = parseInt(document.getElementById("countTo").value);
-
-  const numArr = []
-  for (let index = 0; index <= countTo; index += 1) {
-    const str = index.toString();
-    if (str.includes("3") === true) {
-      numArr.push("Won't you be my neighbor?");
-    } else if (str.includes("2") === true) {
-      numArr.push("Boop!")
-    } else if (str.includes("1") === true) {
-      numArr.push("Beep!")
-    } else {
-      numArr.push(index);
-    }
+function counting(countTo) {
+const numArr = []
+for (let index = 0; index <= countTo; index +=1) {
+  const str = index.toString();
+  if(str.includes("3") === true) {
+    numArr.push("Won't you be my neighbor?");
+  } else if (str.includes("2") === true) {
+    numArr.push("Beep!");
+  }  else if (str.includes("1") === true) {
+      numArr.push("Boop!");
+  } else {
+    numArr.push(index);
   }
-  // ui
+  }
+  return numArr;
+}
+// ui
+function handlesEvent(event) {
+  event.preventDefault();
+  const userInput = parseInt(document.getElementById("countTo").value);
+  const numArr = counting(userInput);
   document.getElementById("result").innerText = numArr
-};
-
-
-
-
+}
